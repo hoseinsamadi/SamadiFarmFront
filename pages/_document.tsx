d008ab6eccdd11a4b628340131f490037fef0dcb
Hosein Samadi <husein.samadi@gmail.com>
2026-09-03 23:42:45 +0330

    feat: set Persian RTL document shell

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fa" dir="rtl">
+      <Head>
+        <meta name="description" content="صمدی فارم | عسل طبیعی و محصولات زنبور عسل" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
